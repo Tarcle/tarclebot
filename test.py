bbb8a4a0fca04540c4133b28579e5066adb151d3
Tarcle <451575@naver.com>
2019-04-02 18:16:15 +0900

    test

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 #https://public-api.tracker.gg/apex/v1/standard/profile/5/Tarcle
+#1e913677-7f9e-491d-b16b-fb097d0456ac
 
 import discord
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
 import json
-
-search = "Tarcle"
-req = urllib.request.Request("https://public-api.tracker.gg/apex/v1/standard/profile/5/"+search, headers={'User-Agent': 'Mozilla/5.0', 'TRN-Api-Key': '1e913677-7f9e-491d-b16b-fb097d0456ac'})
+url = "http://"+urllib.parse.quote("www.mhwdb.kr/apis/weapons/쌍검")
+req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = urllib.request.urlopen(req).read().decode('utf-8')
-# soup = BeautifulSoup(html, 'html.parser')
-
-data = json.loads(html)
-print(data)
+monsters = json.loads(html)
+print(monsters)
