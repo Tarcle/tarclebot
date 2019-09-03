c30e9e1ee0191b9db9818e76a3395c6b2b4be4fb
Tarcle <451575@naver.com>
2019-09-03 18:06:18 +0900

    오큘계정, 서포터계정 검색 오류 수정

--- scoresaber.py
+++ scoresaber.py
@@ -170,10 +170,12 @@ def saveProfile(uid, rankid):
 
 def createProfile(soup, href):
     avatar = soup.select('.avatar>img')[0].get('src')
+    if avatar.startswith('/'):
+        avatar = 'https://scoresaber.com' + avatar
     country = 'https://scoresaber.com'+soup.select('.title>img')[0].get('src')
     name = soup.select('.title')[0].text.strip()
     info = soup.select('.column>ul>li')
-    if len(soup.select('.avatar>center>img'))>0: del info[0]
+    if len(info[0].select('a')) == 0: del info[0]
 
     rank_global = info[0].select('a')[0].text.strip()
     rank_country = info[0].select('a')[1].text.strip()
